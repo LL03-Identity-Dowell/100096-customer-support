48223747fb168ce92592981f7bc5a140b859a2f1
aklile-yilma <aklileyilma@gmail.com>
2024-01-11 01:08:20 +0300

    initial project-setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import ChatPage from './pages/ChatPage'
+
+function App() {
+
+  return (
+    <div className='w-full h-full'>
+      <ChatPage />
+    </div>
+  )
+}
+
+export default App
